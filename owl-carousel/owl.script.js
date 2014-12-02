922298406513cb1df176486ac8e4930382e711e7
gojak <gojak@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-02 08:57:22 +0000

    Version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/mykraft-owl-slider/trunk@1036538 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ owl-carousel/owl.script.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($) {
+    $("#owl-container").owlCarousel({
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        autoPlay : 3000,
+        stopOnHover : true,
+        navigation: false,
+        paginationSpeed : 1000,
+        goToFirstSpeed : 2000,
+        autoHeight : true,
+        autoPlay : true,
+        singleItem:true
+    });
+           
+
+
+});
